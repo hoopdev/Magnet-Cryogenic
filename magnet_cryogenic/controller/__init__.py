7db3d16b937056a62c8628e59a576f0379aa3841
ktaga <taga@issp.u-tokyo.ac.jp>
2021-08-20 11:32:46 +0900

    draft update

--- magnet_cryogenic/controller/__init__.py
+++ magnet_cryogenic/controller/__init__.py
@@ -3,17 +3,23 @@ import pyvisa
 import time
 import dataclasses
 import numpy as np
+import datetime
 
 
 class MagnetOutput(NamedTuple):
-    timestamp: str
+    timestamp: datetime.datetime
     output: float
     voltage: float
 
 
 class MagnetValue(NamedTuple):
-    timestamp: str
+    timestamp: datetime.datetime
     value: float
+
+
+class MagnetPolarity(NamedTuple):
+    timestamp: datetime.datetime
+    value: str
 
 
 class RampStatus(NamedTuple):
@@ -24,11 +30,7 @@ class RampStatus(NamedTuple):
 
 class HeaterStatus(NamedTuple):
     switch: bool
-    field: float
-
-
-class PersistentStatus(NamedTuple):
-    state: bool
+    persistent: bool
     field: float
 
 
@@ -47,28 +49,62 @@ class Controller:
     _inst: Any = dataclasses.field(default=None, init=False)
 
     _output: MagnetOutput = dataclasses.field(default=None, init=False)
+    _record: list = dataclasses.field(default_factory=list, init=False)
     _heater_voltage: MagnetValue = dataclasses.field(default=None, init=False)
     _mid: MagnetValue = dataclasses.field(default=None, init=False)
+    _max: MagnetValue = dataclasses.field(default=None, init=False)
     _ramp_rate: MagnetValue = dataclasses.field(default=None, init=False)
+    _polarity: MagnetPolarity = dataclasses.field(default=None, init=False)
 
     _heater: HeaterStatus = dataclasses.field(default=None, init=False)
     _ramp: RampStatus = dataclasses.field(default=None, init=False)
-    _persistent: PersistentStatus = dataclasses.field(default=PersistentStatus(False, None), init=False)
-    _log: str = dataclasses.field(default=None, init=False)
+    _log: str = dataclasses.field(default="", init=False)
 
     def __post_init__(self) -> None:
         self._resource = pyvisa.ResourceManager()
         self._inst = self._resource.open_resource(self.MAG_ADDRESS)
         self._inst.read_termination = '\r\n'
+        self._inst.write_termination = '\r\n'
         self._inst.timeout = self.TIMEOUT
+        self._inst.delay = 0.25
+        self.output
+        self.heater
+        self.mid
+        self.max
+        self.ramp_rate
+        self.polarity
+
+    def log(self, sentence) -> None:
+        self._log += (str(sentence) + '\n')
+
+    def start_record(self) -> None:
+        while True:
+            time.sleep(0.5)
+            self._record.append(self.output)
+
+    def clear_record(self) -> None:
+        self._record = []
+
+    def record_ramping(self) -> None:
+        update_flag = True
+        count_flag = False
+        count = 0
+        while update_flag:
+            time.sleep(0.5)
+            self._record.append(self.output)
+            count_flag = True if self.ramp_status.state == 'HOLDING' else False
+            if count_flag:
+                count += 1
+            if count > 10:
+                update_flag = False
 
     @property
     def output(self) -> MagnetOutput:
         time.sleep(self.SLEEP)
         self._retry_count = 0
         while self._retry_count < self.RETRY_MAX:
-            self._response = self._inst.query('GET OUTPUT\r')
-            self._log += self._response
+            self._response = self._inst.query('GET OUTPUT')
+            self.log(self._response)
             res_array = self._response.split(' ')
             if res_array[1] == 'OUTPUT:':
                 break
@@ -76,7 +112,7 @@ class Controller:
             time.sleep(self.SLEEP)
 
         try:
-            self._output = MagnetOutput(res_array[0], float(res_array[2]), float(res_array[5]))
+            self._output = MagnetOutput(datetime.datetime.now(), float(res_array[2]), float(res_array[5]))
         except:
             self._output = MagnetOutput(None, None, None)
         return self._output
@@ -84,10 +120,10 @@ class Controller:
     @property
     def mid(self) -> float:
         time.sleep(self.SLEEP)
-        self._response = self._inst.query('GET MID\r')
-        self._log += self._response
+        self._response = self._inst.query('GET MID')
+        self.log(self._response)
         res_array = self._response.split(' ')
-        self._mid = MagnetValue(res_array[0], float(res_array[4]))
+        self._mid = MagnetValue(datetime.datetime.now(), float(res_array[4]))
         return self._mid.value
 
     @mid.setter
@@ -99,31 +135,78 @@ class Controller:
         elif value <= 0:
             print('MID should be positive')
             return
-        self._response = self._inst.query(f'SET MID {value}\r')
-        self._log += self._response
-        print(self._response)
+        self._response = self._inst.query(f'SET MID {value}')
+        print(f'MID set to {str(self.mid)}')
+        self.record_ramping()
+        self.log(self._response)
+        return
+
+    @property
+    def max(self) -> float:
+        time.sleep(self.SLEEP)
+        self._response = self._inst.query('GET MAX')
+        self.log(self._response)
+        res_array = self._response.split(' ')
+        self._max = MagnetValue(datetime.datetime.now(), float(res_array[4]))
+        return self._max.value
+
+    # @max.setter
+    # def max(self, value: float) -> None:
+        # time.sleep(self.SLEEP)
+        # self._response = self._inst.query(f'SET MAX {value}')
+        # self.log(self._response)
+        # print(self._response)
+        # return
+
+    @property
+    def polarity(self) -> float:
+        time.sleep(self.SLEEP)
+        self._response = self._inst.query('GET SIGN')
+        self.log(self._response)
+        res_array = self._response.split(' ')
+        if res_array[1] == 'CURRENT' and res_array[2] == 'DIRECTION:':
+            self._polarity = MagnetPolarity(datetime.datetime.now(), res_array[3])
+        else:
+            raise ValueError("Polarity Error")
+        return self._polarity.value
+
+    @polarity.setter
+    def polarity(self, value: str) -> None:
+        if self.ramp_status.state == 'HOLDING':
+            if value == '+':
+                self._response = self._inst.write(f'DIRECTION {value}')
+                print(self.polarity)
+            elif value == '-':
+                self._response = self._inst.write(f'DIRECTION {value}')
+                print(self.polarity)
+            else:
+                raise ValueError("Set valid polarity")
+        else:
+            print("Hold On")
+
+        self.log(self._response)
         return
 
     @property
     def ramp_rate(self) -> float:
         time.sleep(self.SLEEP)
-        self._response = self._inst.query('GET RATE\r')
-        self._log += self._response
+        self._response = self._inst.query('GET RATE')
+        self.log(self._response)
         res_array = self._response.split(' ')
-        self._ramp_rate = MagnetValue(res_array[0], float(res_array[4]))
+        self._ramp_rate = MagnetValue(datetime.datetime.now(), float(res_array[4]))
         if self._ramp_rate.value == self.PROPER_RAMP_RATE:
-            print("Ramp Rate OK")
+            self.log("Ramp Rate OK")
         else:
-            print("Ramp Rate NG")
+            self.log("Ramp Rate NG")
         return self._ramp_rate.value
 
     @property
     def heater_voltage(self) -> float:
         time.sleep(self.SLEEP)
-        self._response = self._inst.query('GET HV\r')
-        self._log += self._response
+        self._response = self._inst.query('GET HV')
+        self.log(self._response)
         res_array = self._response.split(' ')
-        self._heater_voltage = MagnetValue(res_array[0], float(res_array[4]))
+        self._heater_voltage = MagnetValue(datetime.datetime.now(), float(res_array[4]))
         return self._heater_voltage.value
 
     @property
@@ -131,79 +214,44 @@ class Controller:
         time.sleep(self.SLEEP)
         self._retry_count = 0
         while self._retry_count < self.RETRY_MAX:
-            self._response = self._inst.query('HEATER\r')
-            self._log += self._response
+            self.log(str(self._retry_count))
+            self._response = self._inst.query('HEATER')
+            self.log(self._response)
             res_array = self._response.split(' ')
-            if res_array[1] == 'HEATER' and res_array[2] == 'STATUS':
+            if res_array[1] == 'HEATER' and res_array[2] == 'STATUS:':
                 break
             self._retry_count += 1
             time.sleep(self.SLEEP)
         if res_array[3] == 'ON':
-            self._heater = HeaterStatus(True, None)
+            self._heater = HeaterStatus(True, False, None)
             return self._heater.switch
         elif res_array[3] == 'OFF':
-            self._heater = HeaterStatus(False, None)
+            self._heater = HeaterStatus(False, False, None)
             return self._heater.switch
-        elif res_array[3] == 'SWITCHED' and res_array[4] == 'ON':
-            self._heater = HeaterStatus(True, float(res_array[6]))
-            return self._heater.switch
+        # elif res_array[3] == 'SWITCHED' and res_array[4] == 'ON':
+        #     self._heater = HeaterStatus(True, float(res_array[6]))
+        #     return self._heater.switch
         elif res_array[3] == 'SWITCHED' and res_array[4] == 'OFF':
-            self._heater = HeaterStatus(False, float(res_array[6]))
+            self._heater = HeaterStatus(False, True, float(res_array[6]))
             return self._heater.switch
         else:
             print(self._response)
-            raise ValueError("Error")
+            raise ValueError("Heater Error")
 
     @heater.setter
     def heater(self, value: bool) -> None:
         def heater_on() -> None:
-            self._retry_count = 4
-            while self._retry_count < self.RETRY_MAX:
-                self._response = self._inst.query('HEATER ON\r')
-                self._log += self._response
-                res_array = self._response.split(' ')
-                if res_array[1] == 'HEATER':
-                    break
-                self._retry_count += 1
-                time.sleep(self.SLEEP)
-            if res_array[3] == 'ON':
-                print("Heater ON Started")
-                self._heater = HeaterStatus(True, None)
-            elif res_array[3] == 'SWITCHED' and res_array[4] == 'ON':
-                print("Heater ON Started")
-                self._heater = HeaterStatus(True, float(res_array[6]))
-            else:
-                print('Failed')
-                print(self._response)
-                return
-
+            self._response = self._inst.query('HEATER ON')
+            self.log(self._response)
+            print(self.heater)
             time.sleep(self.HEATER_WAIT)
-            print('Heater ON Finished')
             return
 
         def heater_off() -> None:
-            self._retry_count = 4
-            while self._retry_count < self.RETRY_MAX:
-                self._response = self._inst.query('HEATER OFF\r')
-                self._log += self._response
-                res_array = self._response.split(' ')
-                if res_array[1] == 'HEATER':
-                    break
-                self._retry_count += 1
-                time.sleep(self.SLEEP)
-            if res_array[3] == 'OFF':
-                print("Heater OFF Started")
-                self._heater = HeaterStatus(False, None)
-            elif res_array[3] == 'SWITCHED' and res_array[4] == 'OFF':
-                print("Heater OFF Started")
-                self._heater = HeaterStatus(False, float(res_array[6]))
-            else:
-                print('Failed')
-                print(self._response)
-                return
-
+            self._response = self._inst.query('HEATER OFF')
+            self.log(self._response)
+            print(self.heater)
             time.sleep(self.HEATER_WAIT)
-            print('Heater OFF Finished')
             return
 
         time.sleep(self.SLEEP)
@@ -212,12 +260,11 @@ class Controller:
                 if self.heater:
                     print("Heater is already ON")
                 else:
-                    if self.persistent_status:
-                        if np.abs(self.ramp_status.field - self.persistent_field) > 0.0001:
+                    if self._heater.persistent:
+                        if np.abs(self.ramp_status.field - self._heater.field) > 0.0001:
                             print("Ramp to persistent field first")
                         else:
                             heater_on()
-                            self.persistent_status = False
                     else:
                         heater_on()
 
@@ -225,14 +272,8 @@ class Controller:
                 if not self.heater:
                     print("Heater is already OFF")
                 else:
-                    if self.persistent_status:
-                        print("Persistent status wrong")
-                    else:
-                        if self.ramp_status.field > 0:
-                            heater_off()
-                            self.persistent_status = True
-                        else:
-                            heater_off()
+                    assert not self._heater.persistent
+                    heater_off()
         else:
             print(self._response)
             print("Hold on")
@@ -243,40 +284,31 @@ class Controller:
         time.sleep(self.SLEEP)
         self._retry_count = 0
         while self._retry_count < self.RETRY_MAX:
-            self._response = self._inst.query('RAMP STATUS\r')
-            self._log += self._response
+            self._response = self._inst.query('RAMP STATUS')
+            self.log(self._response)
             res_array = self._response.split(' ')
-            if res_array[1] == 'RAMP':
+            if res_array[1] == 'RAMP' and res_array[2] == 'STATUS:':
                 break
             self._retry_count += 1
             time.sleep(self.SLEEP)
-        if res_array[3] == 'HOLDING':
-            self._ramp = RampStatus('HOLDING', float(res_array[7]), None)
-        elif res_array[3] == 'RAMPING':
-            self._ramp = RampStatus('RAMPING', float(res_array[5]), float(res_array[7]))
+        if res_array[1] == 'RAMP' and res_array[2] == 'STATUS:':
+            if res_array[3] == 'HOLDING':
+                self._ramp = RampStatus('HOLDING', float(res_array[7]), None)
+            elif res_array[3] == 'RAMPING':
+                self._ramp = RampStatus('RAMPING', float(res_array[5]), float(res_array[7]))
+            else:
+                self._ramp = RampStatus(res_array[3], None, None)
+                raise ValueError("Ramping Error")
         else:
-            print("Abnormal status")
-            self._ramp = RampStatus(res_array[3], None, None)
+            raise ValueError("Read Error")
         return self._ramp
-
-    @property
-    def persistent_status(self) -> bool:
-        return self._persistent.state
-
-    @persistent_status.setter
-    def persistent_status(self, state: bool) -> None:
-        self._persistent = PersistentStatus(state, self._heater.field)
-        return
-
-    @property
-    def persistent_field(self) -> float:
-        return self._persistent.field
 
     def ramp_zero(self) -> None:
         time.sleep(self.SLEEP)
         if self.ramp_status.state == 'HOLDING':
-            self._response = self._inst.write('RAMP ZERO\r')
-            self._log += self._response
+            self._response = self._inst.write('RAMP ZERO')
+            self.record_ramping()
+            self.log(self._response)
         elif self.ramp_status.state == 'RAMPING':
             print("Already ramping")
         return
@@ -284,8 +316,9 @@ class Controller:
     def ramp_mid(self) -> None:
         time.sleep(self.SLEEP)
         if self.ramp_status.state == 'HOLDING':
-            self._response = self._inst.write('RAMP MID\r')
-            self._log += self._response
+            self._response = self._inst.write('RAMP MID')
+            self.record_ramping()
+            self.log(self._response)
         elif self.ramp_status.state == 'RAMPING':
             print("Already ramping")
         return
--- magnet_cryogenic/monitor/__init__.py
+++ magnet_cryogenic/monitor/__init__.py
@@ -11,7 +11,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_daq as daq
-import plotly.express as px
 from dash.dependencies import Input, Output
 
 
@@ -33,15 +32,63 @@ class Monitor:
                     id='heater-indicator',
                     label="Heater",
                     color='#ffb6c1',
-                ),),
+                ), width=2),
+                dbc.Col(daq.LEDDisplay(
+                    label="Output (T)",
+                    id='output-display',
+                    color="#ff7f50",
+                    backgroundColor="#708090",
+                    value=""
+                ), width={"size": 3, "offset": 1},),
+                dbc.Col(daq.LEDDisplay(
+                    label="MID (T)",
+                    id='mid-display',
+                    color="#00bfff",
+                    backgroundColor="#708090",
+                    value=""
+                ), width={"size": 3, "offset": 0},),
+                dbc.Col(daq.LEDDisplay(
+                    label="MAX (T)",
+                    id='max-display',
+                    color="#00bfff",
+                    backgroundColor="#708090",
+                    value=""
+                ), width={"size": 3, "offset": 0},),
+                # dbc.Col(daq.LEDDisplay(
+                #     label="Voltage (V)",
+                #     id='voltage-display',
+                #     color="#ff7f50",
+                #     backgroundColor="#708090",
+                #     value=""
+                # ), width={"size": 3, "offset": 0},),
+            ], align="center",),
+            html.Hr(),
+            dbc.Row([
                 dbc.Col(daq.Indicator(
                     id='persistent-indicator',
                     label="Persistent",
                     color='#98fb98',
-                ),),
-            ],),
+                ), width=2),
+                dbc.Col(daq.LEDDisplay(
+                    label="Persistent (T)",
+                    id='persistent-display',
+                    color='#98fb98',
+                    backgroundColor="#708090",
+                    value=""
+                ), width={"size": 3, "offset": 1},),
+                # dbc.Col(daq.LEDDisplay(
+                #     label="Polarity",
+                #     id='polarity-display',
+                #     color='#98fb98',
+                #     backgroundColor="#708090",
+                #     value="+"
+                # ), width={"size": 3, "offset": 1},),
+                dbc.Col(html.H3(id='polarity-display')),
+            ], align="center",),
             html.Hr(),
             dcc.Graph(id="my_graph"),
+
+            dcc.Textarea(id='log-output', style={'width': '90%', 'height': 300},),
             dcc.Interval(
                 id='interval-component',
                 interval=1 * 1000,  # in milliseconds
@@ -51,21 +98,25 @@ class Monitor:
         ])
 
         @self.app.callback(Output("my_graph", "figure"), Input("interval-component", "n_intervals"))
-        def update_graph(selected_country):
-            out = controller.output
-            timestamp.append(datetime.datetime.now())
-            output.append(out.output)
-            voltage.append(out.voltage)
+        def update_graph(n_intervals):
+            record = self.controller._record
+            self.timestamp = []
+            self.output = []
+            self.voltage = []
+            for item in record:
+                self.timestamp.append(item.timestamp)
+                self.output.append(item.output)
+                self.voltage.append(item.voltage)
 
             fig = make_subplots(specs=[[{"secondary_y": True}]])
-            fig.add_trace(
-                go.Scatter(x=timestamp, y=output, name="Output", mode='markers', marker_color="dodgerblue"),
-                secondary_y=False,
-            )
 
             fig.add_trace(
-                go.Scatter(x=timestamp, y=voltage, name="Voltage", mode='markers', marker_color="coral"),
+                go.Scatter(x=self.timestamp, y=self.voltage, name="Voltage", mode='markers', marker_color="silver"),
                 secondary_y=True,
+            )
+            fig.add_trace(
+                go.Scatter(x=self.timestamp, y=self.output, name="Output", mode='markers', marker_color="coral"),
+                secondary_y=False,
             )
 
             xaxis = dict(
@@ -106,17 +157,64 @@ class Monitor:
             Output('heater-indicator', 'value'),
             Input("interval-component", "n_intervals")
         )
-        def update_heater(value):
-            time.sleep(0.2)
-            return True if controller.heater else False
+        def update_heater_indicator(value):
+            return self.controller._heater.switch
 
         @self.app.callback(
             Output('persistent-indicator', 'value'),
             Input("interval-component", "n_intervals")
         )
+        def update_persistent_indicator(value):
+            return self.controller._heater.persistent
+
+        @self.app.callback(
+            Output('mid-display', 'value'),
+            Input("interval-component", "n_intervals")
+        )
+        def update_mid(value):
+            return "{:.3f}".format(self.controller._mid.value)
+
+        @self.app.callback(
+            Output('max-display', 'value'),
+            Input("interval-component", "n_intervals")
+        )
+        def update_max(value):
+            return "{:.3f}".format(self.controller._max.value)
+
+        @self.app.callback(
+            Output('output-display', 'value'),
+            Input("interval-component", "n_intervals")
+        )
+        def update_output(value):
+            return "{:.3f}".format(self.controller._output.output)
+
+        # @self.app.callback(
+        #     Output('voltage-display', 'value'),
+        #     Input("interval-component", "n_intervals")
+        # )
+        # def update_voltage(value):
+        #     return "{:.3f}".format(self.controller._output.voltage)
+
+        @self.app.callback(
+            Output('persistent-display', 'value'),
+            Input("interval-component", "n_intervals")
+        )
         def update_persistent(value):
-            time.sleep(0.2)
-            return True if controller.persistent_status else False
+            return "{:.3f}".format(self.controller._heater.field) if self.controller._heater.persistent else "{:.3f}".format(0)
+
+        @self.app.callback(
+            Output('polarity-display', 'children'),
+            Input("interval-component", "n_intervals")
+        )
+        def update_polarity(value):
+            return f"Polarity:\n{self.controller._polarity.value}"
+
+        @self.app.callback(
+            Output('log-output', 'value'),
+            Input("interval-component", "n_intervals")
+        )
+        def update_log(value):
+            return self.controller._log
 
     def start(self):
         self.app.run_server(mode="jupyterlab")
